027e67640e4e5f2165d278f00f85e862af7ce1e4
Ole Erik Brennhagen <github@elo.anonaddy.com>
2020-05-08 21:25:48 +0200

    Change Cache Settings

--- kupobingo.py
+++ kupobingo.py
@@ -60,6 +60,14 @@ def savimg():
     else:
         return "Invalid Data Sent To server"
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 application = app    
 if __name__ == "__main__":
     application.run("0.0.0.0", 2344)
